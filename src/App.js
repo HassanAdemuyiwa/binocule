84c1eb0fc7e777b364b7223a00b29f02676eab0b
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2021-01-24 17:45:52 +0100

    map viewport set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMapGL, { Maker }from 'react-map-gl';
+// import logo from './logo.svg';
+// import './App.css';
+
+// const Map = ReactMapboxGl({
+//   accessToken:
+//     
+// });
+
 
 function App() {
+
+ const [viewport, setViewport] = React.useState({
+    latitude: 6.497892,
+    longitude: 3.382923,
+    height: '100vh',
+    width: '100vw',
+    zoom: 10
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle= "mapbox://styles/binocule/ckb15bubn1en51iliqpwtmabm"
+      onViewportChange= {viewport => { setViewport(viewport);}}>
+         makers here
+      </ReactMapGL>
+
     </div>
   );
 }
